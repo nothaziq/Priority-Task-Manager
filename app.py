58f7c3f5fc0271fa3b67767eb9112685aaf842d5
nothaziq <muhammadhaziq503123@gmail.com>
2025-12-25 16:22:30 +0500

    Added page configuration.

--- app.py
+++ app.py
@@ -2,3 +2,8 @@ import streamlit as st
 from datetime import datetime
 from DataStructres import PriorityQueue, Stack
 
+st.set_page_config(
+    page_title="Task Manager",
+    page_icon="📝",
+    layout="wide"
+)
